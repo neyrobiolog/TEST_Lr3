a7bd4fab6f37d10e46a99aefca7bd7dcae3349de
neyrobiolog <vpraskovya@bk.ru>
2018-03-11 18:35:59 +0600

    Реализация класса конвертера величин длины. Тест пройден успешно.

---
+++ src/convert/ConLenght.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package convert;
+
+/**
+ * Класс конвертировании величин длины.
+ * @author neyrobiolog
+ */
+public class ConLenght {
+    
+    //Конструктор класса
+    public ConLenght() {};
+    
+}
