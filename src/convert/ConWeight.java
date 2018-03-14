609944ef676ffb6ffe3acd2e56b74ecba9ed2646
neyrobiolog <vpraskovya@bk.ru>
2018-03-14 12:42:06 +0600

    ИСПРАВЛЕНИЯ. Добавлены переменные для обращения к коэффициентам единиц измерения ВЕСА для последующей конвертации

--- src/convert/ConWeight.java
+++ src/convert/ConWeight.java
@@ -14,6 +14,22 @@ public class ConWeight {
     //Конструктор класса
     public ConWeight() {};
     
+    //Переменные для обращения к коэффициентам единиц измерения для конвертирования
+    //Система СИ
+    public static final int GRAMM = 0;      //Грамм
+    public static final int KILOGRAMM = 1;  //Килограмм
+    public static final int TONNA = 2;      //Тонна
+    
+    //Американская система
+    public static final int KVITAL = 3;     //Квитал
+    public static final int KVARTER = 4;    //Квартер
+    public static final int STOUN = 5;      //Стоун
+    
+    //Старорусская система
+    public static final int LOT = 6;        //Лот
+    public static final int ZOLOTNIK = 7;   //Золотник
+    public static final int DOLY = 8;       //Доля
+    
     //Значения перевода величин в килограммы
     public static double[] values = {
         0.001, //Грамм
